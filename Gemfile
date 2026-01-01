26e0633b8fc02ca80bd58cffe6d1e544cbe379a9
Will Richardson <will@willhbr.net>
2026-01-01 09:01:55 +0000

    Add some related posts

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'jekyll', '~> 4.4'
+gem 'kramdown'
 gem 'rouge'
 gem 'jekyll-paginate'
 gem 'webrick'
--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -13,6 +13,17 @@ module CustomFilters
       code.gsub(/<span class=".*?">/, '').gsub('</span>', '')
     end
   end
+
+  def related_posts(post, all_posts, limit)
+    posts = [post.previous, post.next].reject &:nil?
+    return posts unless post['tags'] && all_posts
+    related = all_posts.select do |p|
+      post.url != p.url && (p['tags'] & post['tags']).any? && !posts.any? { |e| e.url == p.url }
+    end
+
+    posts.concat related.first(limit - posts.size)
+    posts.sort_by(&:date).reverse!
+  end
 end
 
 Liquid::Template.register_filter(CustomFilters)
